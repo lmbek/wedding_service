8c1bfea94551cd56f61defc6f812ea1f0933d57d
Lars Morten Bek <lars@beksoft.dk>
2025-05-30 02:10:09 +0200

    Changed main.go and main_test.go

--- main.go
+++ main.go
@@ -1,36 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-var Running bool
+import "fmt"
 
 func main() {
-	Start()
-
-	go Run()
-
-	// Let it run for 1 second
-	time.Sleep(2 * time.Second)
-
-	Stop()
+	start()
 }
 
-func Start() {
-	Running = true
-	fmt.Println("Started")
-}
-
-func Stop() {
-	Running = false
-	fmt.Println("Stopped")
-}
-
-func Run() {
-	for Running {
-		fmt.Println("Running...")
-		time.Sleep(100 * time.Millisecond) // shorter delay for test speed
-	}
+func start() {
+	fmt.Println("coming soon")
 }
--- main_test.go
+++ main_test.go
@@ -1,35 +1,11 @@
 package main
 
-import (
-	"testing"
-	"time"
-)
+import "testing"
 
-func TestRunLifecycle(t *testing.T) {
-	Start()
+func Test_main(t *testing.T) {
+	main()
+}
 
-	done := make(chan struct{})
-	go func() {
-		Run()
-		close(done)
-	}()
-
-	time.Sleep(300 * time.Millisecond) // Let it "run" a bit
-
-	if !Running {
-		t.Error("Expected Running to be true")
-	}
-
-	Stop()
-
-	select {
-	case <-done:
-		// success
-	case <-time.After(1 * time.Second):
-		t.Fatal("Run() did not exit after Stop()")
-	}
-
-	if Running {
-		t.Error("Expected Running to be false after Stop()")
-	}
+func Test_start(t *testing.T) {
+	start()
 }
