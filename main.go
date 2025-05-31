2a2a97243818a2e4b17575843d66334246a37252
Ida Marcher Jensen <54858@student.eadania.dk>
2025-05-31 22:56:19 +0200

    Added webserver start to main.go
    Also added global variables for the servers.
    Also added test to main.go

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"wedding_service/webserver"
+)
+
+var httpServer = &http.Server{Addr: fmt.Sprintf("localhost:%s", os.Getenv("WEDDING_SERVICE_HTTP_PORT"))}
+var httpsServer = &http.Server{Addr: fmt.Sprintf("localhost:%s", os.Getenv("WEDDING_SERVICE_HTTPS_PORT"))}
 
 func main() {
 	start()
 }
 
-func start() {
-	fmt.Println("coming soon")
+func start() error {
+	return webserver.Start(httpServer, httpsServer, filepath.Join("certificate", os.Getenv("LOCALHOST_CERT")), filepath.Join("certificate", os.Getenv("LOCALHOST_KEY")))
 }
--- main_test.go
+++ main_test.go
@@ -1,11 +1,29 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func Test_main(t *testing.T) {
+	go func() {
+		time.Sleep(2 * time.Second)
+		httpServer.Close()
+		httpsServer.Close()
+	}()
+
 	main()
 }
 
 func Test_start(t *testing.T) {
-	start()
+	go func() {
+		time.Sleep(2 * time.Second)
+		httpServer.Close()
+		httpsServer.Close()
+	}()
+
+	err := start()
+	if err != nil {
+		t.Errorf("err: %v", err)
+	}
 }
