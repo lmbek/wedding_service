ab7a2eab4cb7b66a27d07b396fbcbbdbae86b371
Ida Marcher Jensen <54858@student.eadania.dk>
2025-06-01 20:45:49 +0200

    Function Start: * moved setup of the https server to separate file.
    Function ListenAndServe:
    * instead of using waitgroup, and for better handling errors, it was decided to use channels.

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -1,70 +1,41 @@
 package webserver
 
 import (
-	"crypto/tls"
 	"errors"
 	"fmt"
 	"net/http"
-	"os"
-	"sync"
-	"wedding_service/certificate"
 )
 
 func Start(httpServer *http.Server, httpsServer *http.Server, certPath string, keyPath string) error {
-	httpsServer.TLSConfig = &tls.Config{
-		MinVersion: tls.VersionTLS12, // HTTP/2 requires TLS 1.2 or higher
-		NextProtos: []string{"h2"},   // Enforce HTTP/2
-	}
-
-	if os.Getenv("MODE") == "development" {
-		cert, err := certificate.UseLOCALHOST(certPath, keyPath)
-		if err != nil {
-			return fmt.Errorf("could not use localhost certificate: %v\n", err)
-		}
-		httpsServer.TLSConfig.Certificates = []tls.Certificate{*cert}
-	}
-
-	if os.Getenv("MODE") == "production" {
-		autoCertManager := certificate.UseACME()
-		httpsServer.TLSConfig.GetCertificate = autoCertManager.GetCertificate
+	err := setupHttpsServer(httpsServer, certPath, keyPath)
+	if err != nil {
+		return err
 	}
 
 	return ListenAndServe(httpServer, httpsServer)
 }
 
 func ListenAndServe(httpServer *http.Server, httpsServer *http.Server) error {
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	var httpErr, httpsErr error
+	errChannel := make(chan error, 2)
 
 	go func() {
-		defer wg.Done()
 		fmt.Println("Listening on http://" + httpServer.Addr)
 		err := httpServer.ListenAndServe()
-		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("HTTP server error: %s\n", err)
-			httpErr = err
-		}
+		errChannel <- err
 	}()
 
 	go func() {
-		defer wg.Done()
 		fmt.Println("Listening on https://" + httpsServer.Addr)
 		err := httpsServer.ListenAndServeTLS("", "")
-		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("HTTPS server error: %s\n", err)
-			httpsErr = err
-		}
+		errChannel <- err
 	}()
 
-	wg.Wait()
+	for i := 0; i < 2; i++ {
+		err := <-errChannel
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
+	}
 
-	if httpErr != nil {
-		return httpErr
-	}
-	if httpsErr != nil {
-		return httpsErr
-	}
 	return nil
 }
