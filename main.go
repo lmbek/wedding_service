41fb0bee97d32b7dd5ebcd66a5e586ad2740407d
Lars Morten Bek <lars@beksoft.dk>
2025-06-08 04:03:05 +0200

    Refactored main and main_test while also getting 100% test coverage, benchmarks and more
    Lots of changes

--- main.go
+++ main.go
@@ -1,21 +1,49 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"net/http"
-	"os"
-	"path/filepath"
+	"log"
+	"wedding_service/env"
 	"wedding_service/webserver"
 )
 
-var httpServer = &http.Server{Addr: fmt.Sprintf("localhost:%s", os.Getenv("WEDDING_SERVICE_HTTP_PORT"))}
-var httpsServer = &http.Server{Addr: fmt.Sprintf("localhost:%s", os.Getenv("WEDDING_SERVICE_HTTPS_PORT"))}
+var mainWebserver webserver.Webserver
 
+// main starts the program (ignore errors as they are already handled by the other functions in main package
 func main() {
-	start()
+	env.Init()
+	m, _ := initMainWebserver()
+	_ = start(m)
 }
 
-func start() error {
-	// TODO - less parameters
-	return webserver.Start(httpServer, httpsServer, filepath.Join("certificate", os.Getenv("LOCALHOST_CERT")), filepath.Join("certificate", os.Getenv("LOCALHOST_KEY")))
+// start handles the eventual error internally and prints it (main should ignore the error)
+// testWebserver is used only for the tests, for normal use we should just give nil
+func start(w webserver.Webserver) (err error) {
+	if w == nil {
+		return errors.New("webserver is nil")
+	}
+
+	err = w.ListenAndServe()
+	if err != nil {
+		log.Printf("%s", fmt.Errorf("could not ListenAndServe: %w", err))
+		return err
+	}
+	return nil
+}
+
+func initMainWebserver() (webserver.Webserver, error) {
+	if mainWebserver != nil {
+		return mainWebserver, nil
+	}
+
+	m, err := webserver.NewWebserver()
+	if err != nil {
+		if env.IsDebugErrorsEnabled() {
+			log.Printf("%s", fmt.Errorf("could not create new webserver: %w", err))
+		}
+		return nil, err
+	}
+	mainWebserver = m
+	return mainWebserver, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,33 +1,158 @@
 package main
 
 import (
-	"errors"
-	"net/http"
+	"fmt"
 	"testing"
+	"testing/synctest"
 	"time"
+	"wedding_service/env"
+	"wedding_service/webserver"
 )
 
-func Test_main(t *testing.T) {
-	// TODO - use synctest
-	go func() {
-		time.Sleep(2 * time.Second)
-		httpServer.Close()
-		httpsServer.Close()
-	}()
+func Example_main() {
+	env.Init()
 
-	main()
+	w, err := webserver.NewWebserver()
+	if err != nil {
+		fmt.Printf("could not make new webserver: %v", err)
+		return
+	}
+
+	err = w.ListenAndServe()
+	if err != nil {
+		fmt.Printf("%s", fmt.Errorf("could not ListenAndServe: %w", err))
+		return
+	}
+}
+
+func Test_main(t *testing.T) {
+	env.Init()
+
+	// we use synctest to simulate the time.Sleep, but without waiting 500ms
+	synctest.Run(func() {
+		// For main func testing we need to initialize a specific main webserver to test on
+		w, err := webserver.NewWebserver()
+		if err != nil {
+			t.Errorf("could not make new webserver: %v", err)
+			return
+		}
+		// global mainWebserver gets set, so that we can test on it
+		mainWebserver = w
+
+		// close before we start to close the server instantly (use mock testing to test endpoints instead)
+		err = mainWebserver.Close()
+		if err != nil {
+			t.Errorf("could not close webserver: %v", err)
+			return
+		}
+
+		main()
+		resetMainWebserver()
+
+		// this is for having the prints from the test be in the correct place (verbose testing)
+		time.Sleep(500 * time.Millisecond)
+	})
+
 }
 
 func Test_start(t *testing.T) {
-	// TODO - use synctest
-	go func() {
-		time.Sleep(2 * time.Second)
-		httpServer.Close()
-		httpsServer.Close()
-	}()
+	env.Init()
+	// start func tested by running main func test
 
-	err := start()
-	if err != nil && !errors.Is(err, http.ErrServerClosed) { // if the server is closed, we probably forced it
-		t.Errorf("err: %v", err)
+	t.Run("no webserver test", func(t *testing.T) {
+		err := start(nil)
+		if err == nil {
+			t.Errorf("expected an error but got nil")
+		}
+	})
+
+	t.Run("invalid env for webserver", func(t *testing.T) {
+		// change env "mode" to test it with invalid mode (no certificates to be handled)
+		defer env.Reset()
+		env.Env.HttpPort = "-1"
+		env.Env.HttpsPort = "-1"
+
+		w, err := webserver.NewWebserver()
+		if err != nil {
+			t.Errorf("could not create new webserver: %v", err)
+			return
+		}
+
+		err = start(w)
+		if err == nil {
+			t.Errorf("should not be able to start webserver: %v", err)
+			return
+		}
+	})
+}
+
+func BenchmarkStart(b *testing.B) {
+	env.Init()
+	synctest.Run(func() {
+		// use no info debugging
+		defer env.Reset()
+		env.Env.DebugLevel = 2
+
+		for i := 0; i < b.N; i++ {
+			w, err := webserver.NewWebserver()
+			if err != nil {
+				b.Errorf("could not create new webserver: %v", err)
+				return
+			}
+
+			err = w.Close()
+			if err != nil {
+				b.Errorf("could not close webserver: %v", err)
+				return
+			}
+
+			err = start(w)
+			if err != nil {
+				b.Errorf("could not start webserver: %v", err)
+				return
+			}
+		}
+		time.Sleep(500 * time.Millisecond)
+	})
+}
+
+func Test_initMainWebserver(t *testing.T) {
+	env.Init()
+	m, err := initMainWebserver()
+	if err != nil {
+		t.Errorf("could not init webserver: %v", err)
+		return
 	}
+
+	if m == nil {
+		t.Errorf("mainWebserver should not be nil: %v", err)
+		return
+	}
+
+	if mainWebserver == nil {
+		t.Errorf("mainWebserver should not be nil: %v", err)
+		return
+	}
+
+	// reset global variables - this is needed before testing mainWebserver again
+	resetMainWebserver()
+
+	// test fail scenario:
+	t.Run("fail scenario", func(t *testing.T) {
+		// change env "mode" to test it with invalid mode (no certificates to be handled)
+		defer env.Reset()
+		env.Env.Mode = ""
+
+		_, err := initMainWebserver()
+		if err == nil {
+			t.Errorf("initMainWebserver should have failed: %v", err)
+			return
+		}
+		// reset global variables - this is needed before testing mainWebserver again
+		resetMainWebserver()
+	})
+}
+
+func resetMainWebserver() {
+	mainWebserver = nil
 }
