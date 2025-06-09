1dece927ee7c873a4c08e94ddc0e453ddcc84e96
Lars Morten Bek <lars@beksoft.dk>
2025-06-09 21:25:45 +0200

    Split some code into different files

---
+++ webserver/certificate.go
@@ -0,0 +1,36 @@
+package webserver
+
+import (
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"net/http"
+	"wedding_service/certificate"
+	"wedding_service/env"
+)
+
+func useCertificate(httpsServer *http.Server) error {
+	if env.IsModeDevelopment() {
+		cert, err := certificate.UseSelfSigned(env.Env.CertPath, env.Env.KeyPath)
+		if err != nil {
+			return fmt.Errorf("could not use localhost certificate: %w", err)
+		}
+		httpsServer.TLSConfig.GetCertificate = func(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+			return cert, nil
+		}
+	}
+
+	if env.IsModeProduction() {
+		acmeManager, err := certificate.UseAcme()
+		if err != nil {
+			return fmt.Errorf("could not use acme manager: %w", err)
+		}
+		httpsServer.TLSConfig.GetCertificate = acmeManager.GetCertificate
+	}
+
+	if env.IsModeNotSet() {
+		return errors.New("no MODE set in .env")
+	}
+
+	return nil
+}
