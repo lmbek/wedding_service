701c0b992b090882d8a5796f4afd9f2277e0b2e4
Ida Marcher Jensen <54858@student.eadania.dk>
2025-06-09 18:45:03 +0200

    Added variables to the environment struct. The variables are: Hostnames, CertificatePath and KeyPath. Set the new variables in Init. Added function readHostnames

--- env/env.go
+++ env/env.go
@@ -10,18 +10,24 @@ import (
 var Env *environment
 
 type environment struct {
-	DebugLevel int
-	Mode       string
-	HttpPort   string
-	HttpsPort  string
+	DebugLevel      int
+	Mode            string
+	HttpPort        string
+	HttpsPort       string
+	Hostnames       map[string][]string
+	CertificatePath string
+	KeyPath         string
 }
 
 func Init() {
 	Env = &environment{
-		DebugLevel: convertEnvToInt(os.Getenv("DEBUG")),
-		Mode:       strings.ToLower(os.Getenv("MODE")),
-		HttpPort:   os.Getenv("WEDDING_SERVICE_HTTP_PORT"),
-		HttpsPort:  os.Getenv("WEDDING_SERVICE_HTTP_PORT"),
+		DebugLevel:      convertEnvToInt(os.Getenv("DEBUG")),
+		Mode:            strings.ToLower(os.Getenv("MODE")),
+		HttpPort:        os.Getenv("WEDDING_SERVICE_HTTP_PORT"),
+		HttpsPort:       os.Getenv("WEDDING_SERVICE_HTTPS_PORT"),
+		Hostnames:       readHostnames(os.Getenv("WEDDING_SERVICE_HOSTNAMES")),
+		CertificatePath: os.Getenv("LOCALHOST_CERT"),
+		KeyPath:         os.Getenv("LOCALHOST_KEY"),
 	}
 }
 
@@ -72,4 +78,23 @@ func convertEnvToInt(envVar string) int {
 		return -1
 	}
 	return i
+}
+
+func readHostnames(hostnames string) (domainAliases map[string][]string) {
+	domainAliases = make(map[string][]string)
+
+	// Split hostname groups by semicolon
+	hostnameGroups := strings.Split(hostnames, "|")
+	for _, group := range hostnameGroups {
+		// Split hostname and aliases by colon
+		parts := strings.SplitN(group, ":", 2)
+		hostname := parts[0]
+		var aliases []string
+		if len(parts) == 2 {
+			aliases = strings.Split(parts[1], ",")
+		}
+		domainAliases[hostname] = aliases
+	}
+
+	return
 }
