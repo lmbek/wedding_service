db2256b54b118b1c4840f4ad948fc54f606d2224
Lars Morten Bek <lars@beksoft.dk>
2025-06-22 18:16:23 +0200

    Fix: Refactored some code to allow for other modes such as docker-dev
    Before this docker-dev mode did not work

--- webserver/https.go
+++ webserver/https.go
@@ -18,16 +18,16 @@ func newHttpsServer(port string, acmeManager *autocert.Manager) (*http.Server, e
 		NextProtos: []string{"h2"},   // Enforce HTTP/2
 	}
 
+	cert, err := certificate.LoadSelfSigned(env.Env.CertPath, env.Env.KeyPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not use localhost certificate: %w", err)
+	}
+	server.TLSConfig.GetCertificate = func(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+		return cert, nil
+	}
+
 	if env.IsModeDevelopment() {
 		server.Addr = fmt.Sprintf("localhost%s", server.Addr)
-
-		cert, err := certificate.LoadSelfSigned(env.Env.CertPath, env.Env.KeyPath)
-		if err != nil {
-			return nil, fmt.Errorf("could not use localhost certificate: %w", err)
-		}
-		server.TLSConfig.GetCertificate = func(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-			return cert, nil
-		}
 	}
 
 	if env.IsModeProduction() {
