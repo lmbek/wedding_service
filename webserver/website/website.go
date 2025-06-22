93fb04c1ae9bd925333c41f35dc25c450afc26df
Lars Morten Bek <lars@beksoft.dk>
2025-06-22 22:19:09 +0200

    Added html template functionality

--- webserver/website/website.go
+++ webserver/website/website.go
@@ -1,13 +1,31 @@
 package website
 
-import "net/http"
+import (
+	_ "embed"
+	"html/template"
+	"net/http"
+)
+
+//go:embed frontend/out/private/pages/front_page.gohtml
+var frontPageData string
+
+//go:embed frontend/out/private/pages/invitation_page.gohtml
+var invitationPageData string
 
 func FrontPageHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "text/html")
-	w.Write([]byte("kage<style>body { color: red; }</style>"))
+	ExecutePage(
+		w,
+		template.New("frontpage"),
+		frontPageData,
+		map[string]string{"Name": "Lars"},
+	)
 }
 
 func InvitationPageHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "text/html")
-	w.Write([]byte("Invitation<style>body { color: pink; }</style>"))
+	ExecutePage(
+		w,
+		template.New("invitation"),
+		invitationPageData,
+		map[string]string{"Name": "Lars2"},
+	)
 }
