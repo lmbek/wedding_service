4647899724f003fc8ca47a8027a5080439b93a34
lmbek <lars@beksoft.dk>
2025-07-07 19:00:09 +0200

    Updated flags by adding Global variable that can be used for frontend flag

--- flags/flags.go
+++ flags/flags.go
@@ -2,8 +2,15 @@ package flags
 
 import "flag"
 
-var FrontendFlag = flag.String("frontend", "", "Path to frontend files")
+var FrontendFlag string
+var frontendFlag = flag.String("frontend", "", "Path to frontend files")
 
-func Parse() {
+func parse() {
 	flag.Parse()
+}
+
+func LoadFrontendFlag() string {
+	parse()
+	FrontendFlag = *frontendFlag
+	return *frontendFlag
 }
