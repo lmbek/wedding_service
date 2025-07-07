a6de3c2fca8fceda3ff67e8020143a1fa96e2520
lmbek <lars@beksoft.dk>
2025-07-07 18:58:38 +0200

    Added initial frontend files for testing purposes

---
+++ webserver/website/frontend/out/public/gompa-dev.js
@@ -0,0 +1,24 @@
+//import {pageChanger} from "./gladsaxefolkekor";
+
+//import PageChanger from "../components/resources/js/pagechanger/pagechanger.js";
+
+if (!window.socket) {
+    window.socket = new WebSocket('ws://localhost:8080/websocket/hotreload');
+
+    window.socket.onmessage = function(event) {
+        if (event.data) {
+            location.reload()
+            //let pageChanger = new PageChanger("body a[href]")
+            //pageChanger.getPage(location.href, true);
+        } else {
+            // TODO: implement error here, giving error page or something...
+            // insert error page
+            //document.body.innerHTML = event.data;
+        }
+    };
+
+    window.socket.onerror = function(error) {
+        console.log('WebSocket Error:', error);
+    };
+
+}
