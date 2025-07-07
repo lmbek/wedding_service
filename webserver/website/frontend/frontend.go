012a1c3aa1ff87a47346d2d3dec1e02342af032a
lmbek <lars@beksoft.dk>
2025-07-07 19:02:16 +0200

    Added private filesystem and refactored some parts
    Also introduced DefaultFrontend variable for global usage

--- webserver/website/frontend/frontend.go
+++ webserver/website/frontend/frontend.go
@@ -7,6 +7,7 @@ package frontend
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
@@ -14,43 +15,84 @@ import (
 	"wedding_service/flags"
 )
 
-// frontendEmbedded is the embedded frontend for this application so performance is high and dependencies lower
+var DefaultFrontend Frontend = newFrontend()
+
+// frontendEmbedded is the embedded frontend for this application, so performance is high and dependencies lower
+//
+// Example path: go run . -frontend=webserver/website/frontend/out
 //
 //go:embed out/public/*
 var frontendEmbeddedPublic embed.FS
 
+//go:embed out/private/*
+var frontendEmbeddedPrivate embed.FS
+
 type Frontend interface {
-	GetFileSystem() http.FileSystem
+	Serve(w http.ResponseWriter, r *http.Request)
+	GetPublicFileSystem() fs.FS
+	GetPrivateFileSystem() fs.FS
 }
 
 type frontend struct {
-	filesystem *fs.FS
+	publicFilesystem  fs.FS
+	privateFilesystem fs.FS
+	handler           http.Handler
 }
 
 // NewFrontend uses embedding or the flag for the frontend path
-// example: go run . <insert flag with absolute path to directory that holds the frontend>
-func NewFrontend() Frontend {
-	flags.Parse()
+// example: go run . <insert a flag with an absolute path to directory that holds the frontend>
+func newFrontend() Frontend {
+	frontendFlag := flags.LoadFrontendFlag()
 
-	var frontendFS fs.FS
+	var frontendPublicFileSystem fs.FS
+	var frontendPrivateFileSystem fs.FS
+	var err error
 
-	// if frontend flag is set then use files dynamically
-	if *flags.FrontendFlag != "" {
-		frontendFS, err := fs.Sub(os.DirFS(*flags.FrontendFlag), "out/public")
+	// if a frontendFlag is set, then use files dynamically
+	if frontendFlag != "" {
+		fmt.Println("[Frontend] Please note: hotreload enabled. Refresh browser between runs")
+		fmt.Printf("Loading frontend from %s\n", frontendFlag)
+		frontendPublicFileSystem, err = fs.Sub(os.DirFS(frontendFlag), "public")
 		if err != nil {
-			log.Fatalf("frontend path does not lead to a directory: %v\n", err)
+			log.Fatalf("could not load public folder: %v\n", err)
 		}
-		return &frontend{filesystem: &frontendFS}
+		frontendPrivateFileSystem, err = fs.Sub(os.DirFS(frontendFlag), "private")
+		if err != nil {
+			log.Fatalf("could not load private folder: %v\n", err)
+		}
+
+		// start hot reloader
+		go CheckForModification(frontendFlag)
+	} else {
+		fmt.Println("[Frontend] Please note: hotreload disabled. Using embedded frontend! ")
+		// otherwise use embedding
+		frontendPublicFileSystem, err = fs.Sub(frontendEmbeddedPublic, "out/public")
+		if err != nil {
+			log.Fatalf("embedding did not work as we did not have a directory: %v\n", err)
+		}
+		frontendPrivateFileSystem, err = fs.Sub(frontendEmbeddedPrivate, "out/private")
+		if err != nil {
+			log.Fatalf("embedding did not work as we did not have a directory: %v\n", err)
+		}
 	}
 
-	// otherwise use embedding
-	frontendFS, err := fs.Sub(frontendEmbeddedPublic, "out/public")
-	if err != nil {
-		log.Fatalf("embedding did not work as we did not have a directory: %v\n", err)
+	return &frontend{
+		publicFilesystem:  frontendPublicFileSystem,
+		privateFilesystem: frontendPrivateFileSystem,
+		handler: http.FileServer(
+			NewFileSystem(http.FS(frontendPublicFileSystem), false, false),
+		),
 	}
-	return &frontend{filesystem: &frontendFS}
 }
 
-func (f *frontend) GetFileSystem() http.FileSystem {
-	return http.FS(*f.filesystem)
+func (f *frontend) GetPublicFileSystem() fs.FS {
+	return f.publicFilesystem
+}
+
+func (f *frontend) GetPrivateFileSystem() fs.FS {
+	return f.privateFilesystem
+}
+
+func (f *frontend) Serve(w http.ResponseWriter, r *http.Request) {
+	f.handler.ServeHTTP(w, r)
 }
