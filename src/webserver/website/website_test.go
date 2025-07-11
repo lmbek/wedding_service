d9a3a4661c5511f1bc596d73d34434adc9db1c05
lmbek <lars@beksoft.dk>
2025-07-11 13:35:38 +0200

    Commented out old tests

--- src/webserver/website/website_test.go
+++ src/webserver/website/website_test.go
@@ -6,21 +6,21 @@ import (
 	"testing"
 )
 
-func TestFrontPageHandler(t *testing.T) {
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest(http.MethodGet, "/", nil)
-
-	FrontPageHandler(w, r)
-	checkStatusOK(t, w)
-}
-
-func TestInvitationPageHandler(t *testing.T) {
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest(http.MethodGet, "/invitation/", nil)
-
-	InvitationPageHandler(w, r)
-	checkStatusOK(t, w)
-}
+//func TestFrontPageHandler(t *testing.T) {
+//	w := httptest.NewRecorder()
+//	r := httptest.NewRequest(http.MethodGet, "/", nil)
+//
+//	FrontPageHandler(w, r)
+//	checkStatusOK(t, w)
+//}
+//
+//func TestInvitationPageHandler(t *testing.T) {
+//	w := httptest.NewRecorder()
+//	r := httptest.NewRequest(http.MethodGet, "/invitation/", nil)
+//
+//	InvitationPageHandler(w, r)
+//	checkStatusOK(t, w)
+//}
 
 func checkStatusOK(t *testing.T, w *httptest.ResponseRecorder) {
 	if w.Code != http.StatusOK {
